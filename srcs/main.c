bbbc1c1079dac63098770dfbb9821b1fa3eb6af0
tiboitel <tiboitel@student.42.fr>
2016-05-12 18:19:12 +0200

    Window is coming...

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tiboitel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/04 05:19:31 by tiboitel          #+#    #+#             */
-/*   Updated: 2016/05/12 18:00:39 by tiboitel         ###   ########.fr       */
+/*   Updated: 2016/05/12 18:18:27 by tiboitel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,22 @@
 int main(void)
 {
 	SDL_Window	*pWindow;
-	
+	SDL_Renderer *renderer;
+
 	pWindow = NULL;
+	renderer = NULL;
 	if (SDL_Init(SDL_INIT_VIDEO != 0))
 	{
-		fprintf(stdout, "Echec de l'initialisation de la SDL (%s0\n", SDL_GetError());
+		dprintf(2, "Echec de l'initialisation de la SDL (%s0\n", SDL_GetError());
 		return (-1);
 	}
 	pWindow = SDL_CreateWindow("TEST", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == NULL)
+	{
+		dprintf(2, "Echec de l'initialisation du renderer (%s)\n", SDL_GetError());
+		return (-1);
+	}
 	if (pWindow)
 	{
 		SDL_Delay(3000);
